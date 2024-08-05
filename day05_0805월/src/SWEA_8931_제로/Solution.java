3e25dee5936b01b6d0b16291369a35603043706d
hyenem <gpals04291@gmail.com>
2024-08-05 20:38:17 +0900

    0805

--- day05_0805월/src/SWEA_8931_제로/Solution.java
+++ day05_0805월/src/SWEA_8931_제로/Solution.java
@@ -5,30 +5,6 @@ import java.io.FileInputStream;
 import java.util.List;
 import java.util.ArrayList;
 
-class Stack{
-    static List<Integer> stack = new ArrayList<>();
-    static int top =-1;
-    
-    static void push(int i){
-        stack.add(i);
-        top++;
-	}
-    
-    static int pop(){
-        int result = stack.get(top);
-        stack.remove(top--);
-   		return result;
-    }
-    
-    static int peek(){
-        return stack.get(top);
-    }
-    
-    static boolean isEmpty(){
-        return top==-1;
-    }
-}
-
 class Solution
 {
 	public static void main(String args[]) throws Exception
@@ -36,22 +12,46 @@ class Solution
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
-
+		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int K = sc.nextInt();
-            Stack stack = new Stack();
-            for(int i = 0; i<K; i++){
-                int res = sc.nextInt();
-                if(res==0) stack.pop();
-                else stack.push(res);
-            }
-            
-            int sum=0;
-            while(!stack.isEmpty()){
-                sum+=stack.pop();
-            }
-            System.out.println("#"+test_case+" "+sum);
+			Stack stack = new Stack();
+			for(int i = 0; i<K; i++){
+				int res = sc.nextInt();
+				if(res==0) stack.pop();
+				else stack.push(res);
+			}
+			
+			int sum=0;
+			while(!stack.isEmpty()){
+				sum+=stack.pop();
+			}
+			System.out.println("#"+test_case+" "+sum);
 		}
 	}
+}
+
+class Stack{
+    static List<Integer> stack = new ArrayList<Integer>();
+    static int top =-1;
+    
+    void push(int i){
+        stack.add(i);
+        top++;
+	}
+    
+    int pop(){
+        int result = stack.get(top);
+        stack.remove(top--);
+   		return result;
+    }
+    
+    int peek(){
+        return stack.get(top);
+    }
+    
+    boolean isEmpty(){
+        return top==-1;
+    }
 }
