574ab4b4d54db04a7f52d57800d038fd4456422e
hyenem <gpals04291@gmail.com>
2024-08-16 15:07:43 +0900

    0816

--- day11_0814수/src/SWEA_2115_벌꿀채취/Solution.java
+++ day11_0814수/src/SWEA_2115_벌꿀채취/Solution.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
-	
+
 	static int N;
 	static int M;
 	static int C;
 	static int ans;
 	static ArrayList<Integer>[] listArr = new ArrayList[2];
 	static int[][] arr;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
@@ -25,15 +25,15 @@ public class Solution {
 					arr[i][j]=sc.nextInt();
 				}
 			}
-			
+
 			listArr[0]=new ArrayList<Integer>();
 			listArr[1]=new ArrayList<Integer>();
-			
+
 			ans = 0;
-			
+
 		}
 	}
-	
+
 	static void dfs(int i, int j, int sum, int count) {
 		if(count==2) {
 			// 두 명이 다 채집을 마쳤으면 이윤을 계산해서 최대값 업데이트
@@ -41,15 +41,15 @@ public class Solution {
 		}
 		// 아직 두 명이 다 채집을 마치지 않았는데 모든 행을 다 썼으면 리턴
 		if(i>=N) return;
-		// 아직 두 명이 다 채집을 마치지 않았는데 열이 끝나면
-		// 다음 줄로 내려가기
-		if(j>=N) {
-			listArr[count]=new ArrayList<Integer>();
-			dfs(i+1, 0, 0, count);
-		} else if (listArr[count].size()==M) {
+		if (listArr[count].size()==M) {
 			// 한 명이 M개를 다 채집했으면
 			// 다음 사람으로 넘어감
 			dfs(i, j, 0, count+1);
+		} else if(j>=N) {
+			// 아직 채집을 마치지 않았는데 열이 끝나면
+			// 다음 줄로 내려가기
+			listArr[count]=new ArrayList<Integer>();
+			dfs(i+1, 0, 0, count);
 		} else {
 			dfs(i,j+1, sum, count);
 			listArr[count].add(arr[i][j]);
